763152894685e0bedbe974e7abeddc245a1cf0a4
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-21 05:00:48 +0000

    [Add]いいね機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,15 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorites,dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  def favorite_exist?(user)
+    if favorites.find_by(user_id: user.id)
+      true
+    else
+      false
+    end
+  end
+
 end
