1c88e298b53ac30ff5dd3ba3bc63a55caa1e504c
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-23 07:56:51 +0000

    [Finish]応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
   has_many :following,through: :active_relationships, source: :followed
-  has_many :followes, through: :passive_relationships,source: :follower
+  has_many :followers, through: :passive_relationships,source: :follower
 
   has_one_attached :profile_image
 
