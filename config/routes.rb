075b8c6547bcc510f98b27a1e034c5531508df9d
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-23 06:36:52 +0000

    [Add]フォロー機能実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
     resources :comments,only: [:create,:destroy]
     resource :favorites,only: [:create,:destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    member do
+      get :following,:followers
+    end
+  end
+  resources :relationships, only: [:create,:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
