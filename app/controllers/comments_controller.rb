2a098d60a7b3fa491ee8886a2fb13998ef24a674
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-29 07:47:35 +0000

    [Update]応用課題6-コメント機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,19 @@
 class CommentsController < ApplicationController
   def create
-    comment=Comment.new(comment_params)
-    comment.user_id=current_user.id
-    comment.book_id=params[:book_id]
-    comment.save
-    redirect_to request.referer
+    @comment=Comment.new(comment_params)
+    @comment.user_id=current_user.id
+    @comment.book_id=params[:book_id]
+    @comment.save
+    # binding pry
+    # redirect_to request.referer
+    @book=Book.find(params[:book_id])
   end
 
   def destroy
     comment=Comment.find(params[:id])
+    @book=Book.find(comment.book_id)
     comment.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def comment_params
--- app/javascript/script.js
+++ app/javascript/script.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
     $('.favorite-btn').removeClass('favorite-btn-action');
     $(this).addClass('favorite-btn-action');
   });
+  $('.delete-comment-target').on('click',function(){
+    $(this).parent().parent().addClass('row-delete-comment-target');
+  });
 });
+
