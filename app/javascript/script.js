76150d58b737f1e9d59f65dece5f62b4231f0487
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-29 09:19:02 +0000

    [Finish]応用課題6

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,10 +1,34 @@
-$(document).ready(function(){
+// $(document).ready(function(){
+//   $('.favorite-btn').on('click',function(){
+//     $('.favorite-btn').removeClass('favorite-btn-action');
+//     $(this).addClass('favorite-btn-action');
+//   });
+//   $('.delete-comment-target').on('click',function(){
+//     $(this).parent().parent().addClass('row-delete-comment-target');
+//   });
+// });
+
+
+$(document).on('turbolinks:load', function () {
   $('.favorite-btn').on('click',function(){
     $('.favorite-btn').removeClass('favorite-btn-action');
     $(this).addClass('favorite-btn-action');
-  });
-  $('.delete-comment-target').on('click',function(){
-    $(this).parent().parent().addClass('row-delete-comment-target');
+    // event.preventDefault();
   });
 });
 
+// $(document).on('turbolinks:load', function () {
+//   alert("OK?")
+//   $('.delete-comment-target').on('click',function(){
+//     $(this).parent().parent().addClass('row-delete-comment-target');
+//     // event.preventDefault();
+//   });
+// });
+
+
+  $(function(){
+    $('.delete-comment-target').on('click',function(){
+    $(this).parent().parent().addClass('row-delete-comment-target');
+    // event.preventDefault();
+    });
+  });
